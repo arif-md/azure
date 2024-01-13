58739759479a2f289f2a875cfcedc8ddaa690158
Arif Mohammed <arifmohammed.technical@gmail.com>
2024-01-13 23:34:26 +0000

    added .tfvars to isolate the var values

---
+++ terraforms/variables/terraform.tfvars
@@ -0,0 +1,2 @@
+rg-name = "RSG-project-6"
+rg-name2 = "RSG-project-7"
--- terraforms/variables/vars.tf
+++ terraforms/variables/vars.tf
@@ -1,8 +1,10 @@
 variable "rg-name" {
+  description = ""
   type = string
-  default = "RSG-project-4"
+  #default = "RSG-project-4"
 }
 variable "rg-name2" {
+  description = ""
   type = string
-  default = "RSG-project-5"
+  #default = "RSG-project-5"
 }
