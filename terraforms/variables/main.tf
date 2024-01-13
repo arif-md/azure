d43f5a1b6307a6079a91e9fe21f3f27e4af0ec66
Arif Mohammed <arifmohammed.technical@gmail.com>
2024-01-13 22:44:50 +0000

    added terraform and azurerm version

--- terraforms/variables/main.tf
+++ terraforms/variables/main.tf
@@ -1,10 +1,11 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
 terraform {
+  required_version = ">=1.3.2"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = ">=3.0.0"
     }
   }
 }
